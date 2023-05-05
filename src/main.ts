621709d2945653a4a7a8c571f95963ca9f985941
mmaksin <mmaksin@aston.sk>
2023-05-05 14:56:16 +0200

    init commit Vite + Pinia + Tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-createApp(App).use(router).mount("#app");
+import App from './App.vue'
+import router from './router'
+
+import './index.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
